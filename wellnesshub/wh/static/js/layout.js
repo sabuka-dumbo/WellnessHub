cc65110235c7f96f9e431b5b164d96cd58a95481
Sabuka-Dumbo <anonym.sas@proton.me>
2025-03-16 07:52:42 +0000

    Mar 16, 2025, 11:52 AM

---
+++ wellnesshub/wh/static/js/layout.js
@@ -0,0 +1,13 @@
+const navboard = document.getElementById("navboard");
+const nav_div = document.getElementById("nav-div");
+const nav1 = document.getElementById("nav1");
+const nav2 = document.getElementById("nav2");
+const nav3 = document.getElementById("nav3");
+const nav4 = document.getElementById("nav4");
+const burger = document.getElementById("burger");
+const span1 = document.getElementById("span1");
+const span2 = document.getElementById("span2");
+const span3 = document.getElementById("span3");
+
+let cooldown = 0;
+let open = 0
