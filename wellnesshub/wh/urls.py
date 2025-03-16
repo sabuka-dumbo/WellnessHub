20912ae6ada7bb61100d7e552d85a3372a2816a4
Sabuka-Dumbo <anonym.sas@proton.me>
2025-03-16 08:18:48 +0000

    Mar 16, 2025, 12:18 PM

--- wellnesshub/wh/urls.py
+++ wellnesshub/wh/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('/calculator/', views.calculator, name="calculator"),
+    path('calculator/', views.calculator, name="calculator"),
 ]
