c3e9f3aa4a3c9b8c1f07757972a52502c5f2aa3b
Sabuka-Dumbo <anonym.sas@proton.me>
2025-03-17 18:33:00 +0000

    Mar 17, 2025, 10:33 PM

--- wellnesshub/wh/static/js/notes.js
+++ wellnesshub/wh/static/js/notes.js
@@ -20,3 +20,23 @@ function new_note() {
         })
     })
 }
+
+function close_note() {
+    notes_div.style.display = "block";
+    add_notes_div.style.display = "block";
+
+    add_notes_div.style.animation = "fade_out 0.5s ease";
+
+    add_notes_div.addEventListener("animationend", function() {
+        add_notes_div.style.animation = "";
+        add_notes_div.style.display = "none";
+
+        notes_div.style.display = "block"
+        notes_div.style.animation = "fade_in 0.5s ease";
+
+        notes_div.addEventListener("animationend", function() {
+            notes_div.style.animation = '';
+            notes_div.style.display = "block";
+        })
+    })
+}
