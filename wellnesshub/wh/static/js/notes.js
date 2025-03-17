c7fc800cafbd1860a045b41059a24d62c6b16ca0
Sabuka-Dumbo <anonym.sas@proton.me>
2025-03-17 18:35:44 +0000

    Mar 17, 2025, 10:35 PM

--- wellnesshub/wh/static/js/notes.js
+++ wellnesshub/wh/static/js/notes.js
@@ -4,38 +4,67 @@ const add_notes_div = document.getElementById("add-notes-div");
 function new_note() {
     notes_div.style.display = "block";
     add_notes_div.style.display = "block";
-
     notes_div.style.animation = "fade_out 0.5s ease";
 
-    notes_div.addEventListener("animationend", function() {
+    const handleNotesFadeOut = function () {
         notes_div.style.animation = "";
         notes_div.style.display = "none";
 
-        add_notes_div.style.display = "block"
+        add_notes_div.style.display = "block";
         add_notes_div.style.animation = "fade_in 0.5s ease";
 
-        add_notes_div.addEventListener("animationend", function() {
-            add_notes_div.style.animation = '';
-            add_notes_div.style.display = "block";
-        })
-    })
+        const handleAddNotesFadeIn = function () {
+            add_notes_div.style.animation = "";
+            add_notes_div.removeEventListener("animationend", handleAddNotesFadeIn);
+        };
+
+        add_notes_div.addEventListener("animationend", handleAddNotesFadeIn);
+        notes_div.removeEventListener("animationend", handleNotesFadeOut);
+    };
+
+    notes_div.addEventListener("animationend", handleNotesFadeOut);
 }
 
 function close_note() {
-    add_notes_div.style.display = "block";
-
     add_notes_div.style.animation = "fade_out 0.5s ease";
 
-    add_notes_div.addEventListener("animationend", function() {
+    const handleAddNotesFadeOut = function () {
         add_notes_div.style.animation = "";
         add_notes_div.style.display = "none";
 
-        notes_div.style.display = "block"
+        notes_div.style.display = "block";
         notes_div.style.animation = "fade_in 0.5s ease";
 
-        notes_div.addEventListener("animationend", function() {
-            notes_div.style.animation = '';
-            notes_div.style.display = "block";
-        })
-    })
+        const handleNotesFadeIn = function () {
+            notes_div.style.animation = "";
+            notes_div.removeEventListener("animationend", handleNotesFadeIn);
+        };
+
+        notes_div.addEventListener("animationend", handleNotesFadeIn);
+        add_notes_div.removeEventListener("animationend", handleAddNotesFadeOut);
+    };
+
+    add_notes_div.addEventListener("animationend", handleAddNotesFadeOut);
+}
+
+function save_note() {
+    add_notes_div.style.animation = "fade_out 0.5s ease";
+
+    const handleAddNotesFadeOut = function () {
+        add_notes_div.style.animation = "";
+        add_notes_div.style.display = "none";
+
+        notes_div.style.display = "block";
+        notes_div.style.animation = "fade_in 0.5s ease";
+
+        const handleNotesFadeIn = function () {
+            notes_div.style.animation = "";
+            notes_div.removeEventListener("animationend", handleNotesFadeIn);
+        };
+
+        notes_div.addEventListener("animationend", handleNotesFadeIn);
+        add_notes_div.removeEventListener("animationend", handleAddNotesFadeOut);
+    };
+
+    add_notes_div.addEventListener("animationend", handleAddNotesFadeOut);
 }
