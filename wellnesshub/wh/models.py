dc762d635ff658cb3beae95a8e41a51022e804a4
Sabuka-Dumbo <anonym.sas@proton.me>
2025-03-20 16:09:18 +0000

    Mar 20, 2025, 8:09 PM

--- wellnesshub/wh/models.py
+++ wellnesshub/wh/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Note():
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=1500)
+    pics = models.ImageField()
+
+    def __str__(self):
+        return f"self.user added '{self.title}' note"
