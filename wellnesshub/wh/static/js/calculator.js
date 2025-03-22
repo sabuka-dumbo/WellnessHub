14219ba95d62d3774cb5334beb6c4ddd51917a52
Sabuka-Dumbo <anonym.sas@proton.me>
2025-03-22 06:28:04 +0000

    Mar 22, 2025, 10:28 AM

--- wellnesshub/wh/static/js/calculator.js
+++ wellnesshub/wh/static/js/calculator.js
@@ -109,6 +109,55 @@ function leanmasscalculation() {
     })
 }
 
+function BMIcalculator() {
+    calculator_title.innerText = "BMI Calculator:"
+
+    calc_field1.style.opacity = "100%";
+    calc_field2.style.opacity = "100%";
+    calc_field3.style.opacity = "50%";
+    calc_field4.style.opacity = "50%";
+    calc_field5.style.opacity = "50%";
+    calc_field6.style.opacity = "50%";
+
+    calc_field1.value = '';
+    calc_field2.value = '';
+    calc_field3.value = '';
+    calc_field4.value = '';
+    calc_field5.value = '';
+    calc_field6.value = '';
+
+    calc_field1.disabled = false;
+    calc_field2.disabled = false;
+    calc_field3.disabled = true;
+    calc_field4.disabled = true;
+    calc_field5.disabled = true;
+    calc_field6.disabled = true;
+
+    calc_field1.placeholder = "Enter Height";
+    calc_field2.placeholder = "Enter Weight";
+    calc_field3.placeholder = "Enter Neck Circumference";
+    calc_field4.placeholder = "Enter Hip Circumference";
+    calc_field5.placeholder = "Enter Waist Circumference";
+
+    submit.addEventListener("click", function() {
+        let result2 = 0;
+
+        if (MorF1.checked) {
+            result2 = calculateBMI(calc_field1.value, calc_field2.value)
+        } else {
+            result2 = calculateBMI(calc_field1.value, calc_field2.value)
+        }
+
+        let last_result = (Math.floor(result2 * 10) / 10) + " Kg Mass"
+
+        const result2_title = document.getElementById("result2-title");
+        const results_title2 = document.getElementById("results-title2");
+
+        result2_title.innerText = last_result;
+        results_title2.innerText = last_result;
+    })
+}
+
 function calculateBodyFatMen(waist, neck, height) {
     const log10 = Math.log10;
     const denominator = 1.0324 - 0.19077 * log10(waist - neck) + 0.15456 * log10(height);
@@ -137,4 +186,10 @@ function calculateLeanMassMen(height, weight) {
 function calculateLeanMassWomen(height, weight) {
     const eLBM = 0.29569 * weight + 0.41813 * height - 43.2933;
     return eLBM;
+}
+
+function calculateBMI(height, weight) {
+    const heightM = height / 100;
+    const bmi = weight / (heightM * heightM);
+    return Math.round(bmi * 10) / 10;
 }
